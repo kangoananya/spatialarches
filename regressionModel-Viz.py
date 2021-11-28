1fbcf7181f50ebefc37e281de026838c5a97f5d6
ananyakango <49430982+ananyakango@users.noreply.github.com>
2021-11-28 15:07:07 +0100

    save images

--- regressionModel-Viz.py
+++ regressionModel-Viz.py
@@ -44,13 +44,15 @@ U = np.array([x_,y_]).T
 _v = np.array(z_)
 
 #fake regression to match viz
-regr = make_pipeline((PolynomialFeatures(1)),linear_model.LinearRegression())
+regr = linear_model.LinearRegression()
 regr.fit(U,_v)
 
 predicted = regr.predict(model_viz)
 
-fig = plt.figure(figsize=(60,10))
-ax2 = fig.add_subplot(122,projection = '3d')
+print(regr.coef_)
+print(regr.intercept_)
+fig = plt.figure(figsize=(15,15))
+ax2 = fig.add_subplot(111,projection = '3d')
 ax2.set_xlabel('Width', fontsize=12)
 ax2.set_ylabel('Depth', fontsize=12)
 ax2.set_zlabel('Extruder Speed', fontsize=12)
@@ -58,6 +60,9 @@ ax2.set_zlabel('Extruder Speed', fontsize=12)
 ax2.plot(x_, y_, z_, color='k', zorder=15, linestyle='none', marker='o', alpha=0.5)
 ax2.scatter(xx_pred.flatten(), yy_pred.flatten(), predicted, facecolor=(0,0,0,0), s=10, edgecolor='xkcd:deep pink')
 
+#for ii in np.arange(0, 360, 1):
+#    ax2.view_init(elev=32, azim=ii)
+#    fig.savefig('gif_image%d.png' % ii)
 
 #actual regression and deviation calculation
 q_reg = make_pipeline((PolynomialFeatures(1)),linear_model.LinearRegression())
